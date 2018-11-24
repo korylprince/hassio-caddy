1a5124001772804ef4323f8292b1404039dcc5ab
Kory Prince <korylprince@gmail.com>
2018-11-23 20:48:11 -0600

    add ability to run a custom Caddy binary

--- caddy/run.sh
+++ caddy/run.sh
@@ -2,9 +2,18 @@
 set -e
 
 CONFIG_PATH=/data/options.json
+CADDY_SHARE_PATH="/share/caddy"
+CADDY_PATH=/usr/sbin/caddy
 
-ARGS=( "-conf" "/share/caddy/Caddyfile" $(jq --raw-output '.flags[]' $CONFIG_PATH) )
+ARGS=( "-conf" "$CADDY_SHARE_PATH/Caddyfile" $(jq --raw-output '.flags[]' $CONFIG_PATH) )
 
 echo "Running Caddy with arguments: ${ARGS[*]}"
 
-exec /usr/sbin/caddy ${ARGS[*]}
+if [ -f "$CADDY_SHARE_PATH/caddy.bin" ]; then
+    CADDY_PATH="$CADDY_SHARE_PATH/caddy.bin"
+    echo "Found custom Caddy: $($CADDY_PATH -version)"
+else
+    echo "Using built-in Caddy: $($CADDY_PATH -version)"
+fi
+
+exec $CADDY_PATH ${ARGS[*]}
